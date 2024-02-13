556002bf806903ab4e566cb535adf94f2023264f
ChauTin220 <cchan220@myseneca.ca>
2024-02-13 11:27:51 -0500

    Keydown update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,7 @@
 const result = document.getElementById("result");
 const btn = document.getElementById("searchbtn");
+const inputField = document.getElementById("inputword");
+// console.log (inputField);
 
 const options = {
     method: 'GET',
@@ -9,30 +11,35 @@ const options = {
     }
 };
 
-btn.addEventListener("click", () => {
-    const input = document.getElementById("inputword").value;
-    // console.log (input);
+// async/await
+async function getData() {
+    try {
+        const input = inputField.value;
+        const response = await fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${input}`, options);
+        // console.log(response);
+        const infoFromServer = await response.json();
+        // console.log(infoFromServer);
 
-    // async/await
-    async function getData() {
-        try {
-            const response = await fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${input}`, options);
-            // console.log(response);
-            const infoFromServer = await response.json();
-            console.log(infoFromServer);
-
-            result.innerHTML = `
-            <h1>${infoFromServer.list[0].word}</h1>
-            <p>${infoFromServer.list[0].definition}</p>
-            <h2>${infoFromServer.list[0].example}</h2>
-            <h3>By ${infoFromServer.list[0].author} on ${infoFromServer.list[0].written_on}</h3>
-            `;
-        } catch (error) {
-            console.warn(`Nope:${error}`);
-        }
+        result.innerHTML = `
+        <h1>${infoFromServer.list[0].word}</h1>
+        <p>${infoFromServer.list[0].definition}</p>
+        <h2>${infoFromServer.list[0].example}</h2>
+        <h3>By ${infoFromServer.list[0].author} on ${infoFromServer.list[0].written_on}</h3>
+        `;
+    } catch (error) {
+        console.warn(`Nope:${error}`);
     }
+}
 
+// Click
+btn.addEventListener("click", () => {
     getData();
 });
 
-
+// Keydown
+inputField.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        getData();
+    }
+});
